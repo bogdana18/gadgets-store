c279b2da2fa248d07f6b3ae837d87c0cc97f0a4e
Usenko Bohdana <usenkobogdana19@gmail.com>
2023-04-30 14:03:05 +0300

    favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { Helmet } from 'react-helmet';
 import { Page } from './pages/Page';
 import './App.scss';
 
 export const App = () => {
   return (
     <div className="App">
+      <Helmet>
+        <link rel="icon" type="image/svg" href="new/favicon.svg" />
+      </Helmet>
       <Page />
     </div>
   );
